1ae8fed1659900083527c22bac7397465c9b4f8e
Rex <dfzou@qq.com>
2015-09-12 00:42:37 +0800

    增加新Demo

---
+++ activityanimation/src/main/java/com/win16/activityanimation/TransitionFirstActivity.java
@@ -0,0 +1,47 @@
+package com.win16.activityanimation;
+
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.transition.Explode;
+import android.transition.Slide;
+import android.util.Pair;
+import android.view.Gravity;
+import android.view.View;
+
+import com.win16.activityanimation.R;
+
+
+public class TransitionFirstActivity extends Activity {
+
+    private View mFabButton;
+    private View mHeader;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transition_first);
+
+        mFabButton = findViewById(R.id.fab_button);
+        mHeader = findViewById(R.id.activity_transition_header);
+
+        Explode slideExitTransition = new Explode();
+        slideExitTransition.excludeTarget(android.R.id.navigationBarBackground, true);
+        slideExitTransition.excludeTarget(android.R.id.statusBarBackground, true);
+        slideExitTransition.excludeTarget(R.id.activity_transition_header, true);
+        getWindow().setExitTransition(slideExitTransition);
+    }
+
+    public void onFabPressed(View view) {
+
+        Intent i  = new Intent (TransitionFirstActivity.this,
+            TransitionSecondActivity.class);
+
+        ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(
+            TransitionFirstActivity.this,Pair.create(mFabButton, "fab"), Pair.create(mHeader, "holder1"));
+
+        startActivity(i, transitionActivityOptions.toBundle());
+    }
+}
